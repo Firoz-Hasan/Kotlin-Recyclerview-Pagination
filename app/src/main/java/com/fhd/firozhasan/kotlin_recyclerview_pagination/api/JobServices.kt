6d74076ea89ea05742c1349caced9ec2c7433182
firozhasan <diganto007@gmail.com>
2019-01-04 17:16:17 +0600

    commented

--- app/src/main/java/com/fhd/firozhasan/kotlin_recyclerview_pagination/api/JobServices.kt
+++ app/src/main/java/com/fhd/firozhasan/kotlin_recyclerview_pagination/api/JobServices.kt
@@ -14,4 +14,23 @@ interface JobServices {
         @Query("page") pageIndex: Int
     ): Call<PopularMovies>
 
+    /*
+    *
+    *
+    *
+    * what that getTopRatedMovies function means is
+    * when calling this function i will send parameters
+    * like api key, language and page and in return
+    * THE REST API will RETURN PopularMovies object.
+    * In addition that particular PopularMovies object could be
+    * just object or contains list of inner object or something more.
+    * THIS RETURNED OBJECT should be perfectly matched/aligned with the model class that
+    * we have created
+    * @Query means query String / parameter
+    * @Field means body
+    *
+    *
+    *
+    *
+    * */
 }
